c12af2c3c351284af5b849613f9ab5082deff4df
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-14 19:07:24 +0530

    FileServertest

---
+++ jwebserver/FileServerTest.java
@@ -0,0 +1,25 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import com.sun.net.httpserver.SimpleFileServer;
+
+public class FileServerTest{
+
+
+
+    
+    public static void main(String[] args)
+    {
+
+         Path path=Path.of("D:\\git-personal\\java-ee\\Jwebserver\\test_file");
+        HttpServer server=SimpleFileServer.createFileServer(new InetSocketAddress(8000), path, SimpleFileServer.OutputLevel.VERBOSE);
+
+        server.start();
+
+          
+    }
+   
+
+
+}
