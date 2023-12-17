078841a9f7f6c0840d0354420186ccbb68ee2200
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-12-17 22:31:53 +0530

    Update FileServerTest.java

--- jwebserver/FileServerTest.java
+++ jwebserver/FileServerTest.java
@@ -13,7 +13,7 @@ public class FileServerTest{
     
     public static void main(String[] args)
     {
-
+        
         Path path=Path.of("D:\\git-personal\\java-ee\\Jwebserver\\test_file");
         HttpServer server=SimpleFileServer.createFileServer(new InetSocketAddress(8000), path, SimpleFileServer.OutputLevel.VERBOSE);
         server.start();
